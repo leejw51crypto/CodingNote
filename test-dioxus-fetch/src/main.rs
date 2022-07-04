d2a1f0cbcce9dc19f919a99036ac98c5c5de9292
jongwhan <jonghwan@crypto.com>
2022-07-04 12:47:38 +0900

    add mutex

--- test-dioxus-fetch/src/main.rs
+++ test-dioxus-fetch/src/main.rs
@@ -6,29 +6,72 @@ use dioxus::prelude::*;
 use futures_channel::mpsc::{ UnboundedReceiver};
 use futures::StreamExt;
 use anyhow::Result;
-
-
+use rusqlite::{params, Connection};
+use std::sync::{Arc,Mutex};
+// derive with serde
+#[derive(Debug, Clone,Default, serde::Serialize, serde::Deserialize)]
 pub struct MyItem {
     messageid: i64,
     messagetime: String,
     messagetext: String,
 }
 static MESSAGES: AtomRef<BTreeMap<i64, MyItem>> = |_| BTreeMap::new();
-#[derive(Clone, Debug, Default)]
+#[derive( Debug, Clone)]
 pub struct Program {
-    // sqlite database
-    // pub db: AtomRef<SqliteDatabase>,
+    db: Arc<Mutex<Connection>>
+
 }
 impl Program {
     fn new() -> Self {
-        Self::default()
+        println!("create program");
+        let db = Connection::open("db.sqlite").unwrap();
+        db.execute("CREATE TABLE IF NOT EXISTS messages (
+            messageid INTEGER PRIMARY KEY,
+            messagetime TEXT,
+            messagetext TEXT
+        )", params![]).unwrap();
+        Self {
+            db:Arc::new(Mutex::new(db))
+        }
+    }
+    async fn get_message(&self)->rusqlite::Result<Vec<MyItem>> {
+        let db = self.db.lock().unwrap();
+        let mut stmt = db.prepare("SELECT * FROM messages").unwrap();
+        let mut rows = stmt.query(params![]).unwrap();
+        let mut messages = Vec::new();
+        while let Some(row) = rows.next().unwrap() {
+            let messageid: i64 = row.get(0)?;
+            let messagetime: String = row.get(1)?;
+            let messagetext: String = row.get(2)?;
+            messages.push(MyItem {
+                messageid,
+                messagetime,
+                messagetext
+            });
+        }
+        Ok(messages)
     }
     fn initialize(&self)->Result<()> {
+        let db = self.db.lock().unwrap();
+        println!("initialize ----------------------------");
+        let count=2;
+        for i in 0..count {
+            let utctime=chrono::Utc::now();
+            let localtime=utctime.with_timezone(&chrono::Local);
+            
+            let message = MyItem {
+                messageid: utctime.timestamp_nanos(),
+                messagetime: localtime.to_rfc3339(),
+                messagetext: format!("sample text {}", i*10),
+            };
+            db.execute("INSERT INTO messages (messageid, messagetime, messagetext) VALUES (?1, ?2, ?3)",
+                params![message.messageid, message.messagetime, message.messagetext]).unwrap();
+        }
         Ok(())
     }
 }
 pub struct AppProps {
-    pub program: Program,
+    program: Program,
 }
 #[derive( Props)]
 pub struct MainProps {
@@ -44,52 +87,42 @@ impl PartialEq for MainProps {
 #[derive(Clone, Debug, Default)]
 pub struct MyCommand {
     command: String,
-    message: String,
 }
 
-fn start_process(cx: &Scope<AppProps>) -> anyhow::Result<CoroutineHandle<MyCommand>> {
+/// this is called everytime
+fn setup_process(cx: & Scope<AppProps>) -> anyhow::Result<CoroutineHandle<MyCommand>> {
     let names = use_atom_ref(cx, MESSAGES).clone();
+    let myprogram= cx.props.program.clone();
     let task = use_coroutine(cx, |mut rx: UnboundedReceiver<MyCommand>| async move {
-    
-
         while let Some(cmd) = rx.next().await {
             match cmd.command.as_str() {
                 "fetch" => {
-
-                    for i in 0..10 {
-                        let currenttime= chrono::Utc::now();
-                        
-                        let e = MyItem {
-                            messageid: currenttime.timestamp_nanos(),
-                            messagetime: "1".into(),
-                            messagetext: "2".into()
-                        };
-                        names.write().insert(e.messageid, e);
+                    let items=myprogram.get_message().await.unwrap();
+                    for e in &items {
+                        names.write().insert(e.messageid, e.clone());
                     }
                 }
                 _ => {
                     println!("unknown command");
                 }                          
-
             } 
-
         } 
-    });
-
+    }); // end of while
     Ok(task.clone())
-
 }
-
-
 
 fn main() {
-    let program = Program::new();
-    dioxus::desktop::launch_with_props(app, AppProps { program }, |c| c);
+    // setup logic objects, share with props
+    // for thread safety, arc + mutex needed
+    let program= Program::new();
+    program.initialize().unwrap();
+    dioxus::desktop::launch_with_props(app, AppProps {program  }, |c| c);
 }
 
+// this is called very often
 fn app(cx: Scope<AppProps>) -> Element {
-
-    let task=start_process(&cx).unwrap();
+    println!("app element----------------------------");
+    let task=setup_process(&cx).unwrap();
     cx.render(rsx! {
         div { "hello" }
         HomePage{ taskid:"messagestask".into(), task: task}
@@ -104,7 +137,6 @@ fn HomePage(cx: Scope<MainProps>) -> Element {
                 onclick: move |_| {
                     cx.props.task.send(MyCommand {
                         command: "fetch".into(),
-                        message:"".into(),
                     });  
                 },
                 "fetch"
