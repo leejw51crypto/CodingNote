408a07bfa43d5734b61c8a094213157167e6d2c0
jongwhan <jonghwan@crypto.com>
2023-07-10 19:07:17 +0900

    refactor

--- test-vector-database/python/train.py
+++ test-vector-database/python/train.py
@@ -1,107 +1,97 @@
+import os
 import json
-
-import numpy as np
-import pandas as pd
 import torch
-import os 
 import pandas as pd
-
+import numpy as np
+from tqdm import tqdm
+from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
-from sentence_transformers import SentenceTransformer
 
-from tqdm import tqdm
 COLLECTION_NAME="mybook"
+DATA_PATH = "data.json"
+QDRANT_HOST = os.environ.get('QDRANT_HOST')
+BATCH_SIZE = 512
+MODEL_NAME = "msmarco-MiniLM-L-6-v3"
+DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
+def load_data(path):
+    print(f"Loading data from {path}")
+    with open(path, "r") as file:
+        meditations_json = json.load(file)
+    return meditations_json["data"]
 
-with open(f"data.json", "r") as file:
-    meditations_json = json.load(file)
+def create_qdrant_client(host):
+    print(f"Creating Qdrant client with host {host}")
+    client = QdrantClient(host)
+    client.recreate_collection(
+        collection_name=COLLECTION_NAME,
+        vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE),
+    )
+    return client
 
-items= meditations_json["data"]
-print(len(items))
-# iterate items, display title, url
-for item in items:
-    print(f"title={item['title']}, sentences={len(item['sentences'])}")
+def prepare_data(items):
+    print("Preparing data")
+    rows = []
+    for chapter in items:
+        for sentence in chapter["sentences"]:
+            rows.append((chapter["title"], chapter["url"], sentence))
 
+    df = pd.DataFrame(data=rows, columns=["title", "url", "sentence"])
+    df = df[df["sentence"].str.split().str.len() > 15]
+    return df
 
-# Create collection
-qdrant_host = os.environ.get('QDRANT_HOST')
-print(qdrant_host)
-client = QdrantClient(qdrant_host)
-client.recreate_collection(
-    collection_name=COLLECTION_NAME,
-    vectors_config=models.VectorParams(
-    	size=384, 
-    	distance=models.Distance.COSINE
-    ),
-)
+def encode_sentences(df, model_name, device, batch_size):
+    print("Encoding sentences")
+    model = SentenceTransformer(model_name, device)
+    vectors = []
+    batch = []
 
+    for doc in tqdm(df["sentence"].to_list()):
+        batch.append(doc)
+        if len(batch) >= batch_size:
+            vectors.append(model.encode(batch))
+            batch = []
 
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-print(f"Using device: {device}")
-
-
-rows = []
-for chapter in items:
-    for sentence in chapter["sentences"]:
-        rows.append(
-            (
-                chapter["title"],
-                chapter["url"],
-                sentence,
-            )
-        )
-
-df = pd.DataFrame(
-    data=rows, columns=["title", "url", "sentence"]
-)
-
-df = df[df["sentence"].str.split().str.len() > 15]
-print(df)
-
-
-model = SentenceTransformer("msmarco-MiniLM-L-6-v3", device)
-vectors = []
-batch_size = 512
-batch = []
-
-for doc in tqdm(df["sentence"].to_list()):
-    batch.append(doc)
-    
-    if len(batch) >= batch_size:
+    if len(batch) > 0:
         vectors.append(model.encode(batch))
         batch = []
-
-if len(batch) > 0:
-    vectors.append(model.encode(batch))
-    batch = []
     
-vectors = np.concatenate(vectors)
+    vectors = np.concatenate(vectors)
+    return vectors
 
-book_name = meditations_json["book_title"]
-print(df)
-print(f"df {df.shape} vectors {vectors.shape}")
-print(f"payloads {df.shape[0]} vectors {vectors.shape[0]}")
+def create_payloads(df, vectors):
+    print("Creating payloads")
+    ids=[i for i in range(df.shape[0])]
+    payloads=[
+        {
+            "text": row["sentence"],
+            "title": row["title"],
+            "url": row["url"],
+        }
+        for _, row in df.iterrows()
+    ]
+    return ids, payloads
 
-ids=[i for i in range(df.shape[0])]
-payloads=[
-    {
-        "text": row["sentence"],
-        "title": row["title"] + f", {book_name}",
-        "url": row["url"],
-    }
-    for _, row in df.iterrows()
-]
+def upload_data(client, ids, payloads, vectors):
+    print("Uploading data")
+    client.upsert(
+        collection_name=COLLECTION_NAME,
+        points=models.Batch(
+            ids=ids,
+            payloads=payloads,
+            vectors=[v.tolist() for v in vectors],
+        ),
+    )
 
-print(len(ids),ids[:2],"\n...\n",ids[-2:])
-print(len(payloads),payloads[:2],"\n...\n",payloads[-2:])
-client.upsert(
-    collection_name=COLLECTION_NAME,
-    points=models.Batch(
-        ids=ids,
-        payloads=payloads,
-        vectors=[v.tolist() for v in vectors],
-    ),
-)
+def main():
+    items = load_data(DATA_PATH)
+    client = create_qdrant_client(QDRANT_HOST)
+    df = prepare_data(items)
+    vectors = encode_sentences(df, MODEL_NAME, DEVICE, BATCH_SIZE)
+    ids, payloads = create_payloads(df, vectors)
+    upload_data(client, ids, payloads, vectors)
+    print("Uploaded.")
 
-print("uploaded")
+if __name__ == "__main__":
+    main()
