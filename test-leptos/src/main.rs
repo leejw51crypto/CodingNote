379ee7ef9f7ec06b0db5d90588eeaf4cf5f096f1
jonghwan lee <jonghwan@crypto.com>
2023-09-10 21:14:19 +0900

    ok

--- test-leptos/src/main.rs
+++ test-leptos/src/main.rs
@@ -1,5 +1,30 @@
 use leptos::*;
 
-fn main() {
+fn main2() {
     mount_to_body(|cx| view! { cx,  <p>"Hello, world!"</p> })
+}
+
+fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(|cx| view! { cx, <App/> })
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    
+    let (count, set_count) = create_signal(cx, 0);
+    create_effect(cx, move |_| {
+        log::info!("Debug Value: {}", count());
+    });
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count(3);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
 }
