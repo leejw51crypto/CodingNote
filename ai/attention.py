64a818c915f70e8c52da826310529d2631d58d49
jonghwan lee <jonghwan@crypto.com>
2023-10-18 16:56:52 +0900

    tidy up

--- ai/attention.py
+++ ai/attention.py
@@ -15,36 +15,34 @@ def scaled_dot_product_attention(query, key, value):
     """
     
     # Compute the dot product between query and key
-    # The transpose(0, 2, 1) operation is applied to a 3-dimensional numpy array, 
-    # and it rearranges the dimensions of the array according to the specified order.
-    # Given a 3-dimensional array with shape (a, b, c), the transpose(0, 2, 1) operation
-    # will rearrange its dimensions to have a shape of (a, c, b).
     matmul_qk = np.matmul(query, key.transpose(0, 2, 1))
-    print("Shape of Q:\n ->", query.shape)
-    print("Shape of K:\n ->", key.shape)
-    print("Shape of K.transpose(0, 2, 1) ->", key.transpose(0, 2, 1).shape)
-    print("Shape of np.matmul(query, key.transpose(0, 2, 1)) ->", matmul_qk.shape)
-    print("Dot product of Q and K:\n", matmul_qk)
     
     # Scale the dot product by the square root of the depth
     depth = query.shape[-1]
     logits = matmul_qk / np.sqrt(depth)
-    print("Scaled dot product:\n", logits)
     
     # Compute the attention weights using softmax
     attention_weights = np.exp(logits) / np.sum(np.exp(logits), axis=-1, keepdims=True)
-    print("Attention weights:\n", attention_weights)
     
     # Compute the output by multiplying the attention weights with the value matrix
-    # print shape of attention_weights: (2, 4, 4)
-    # print shape of value: (2, 4, 6)
-    # print shape of output: (2, 4, 6)
     output = np.matmul(attention_weights, value)
-    print("Shape of attention_weights:\n ->", attention_weights.shape)
-    print("Shape of value:\n ->", value.shape)
-    print("Shape of np.matmul(attention_weights, value):\n ->", output.shape)
-    print("Output:\n", output)
-    print("Output shape:", output.shape)
+    
+    # Print formatted outputs
+    print("Shapes:")
+    print("- Query (Q):", query.shape)  # Expected: (batch_size, num_positions, depth)
+    print("- Key (K):", key.shape)      # Expected: (batch_size, num_positions, depth)
+    print("- Key Transposed:", key.transpose(0, 2, 1).shape)  # Expected: (batch_size, depth, num_positions)
+    print("- Dot Product (Q.K^T):", matmul_qk.shape)  # Expected: (batch_size, num_positions, num_positions)
+    print("- Attention Weights:", attention_weights.shape)  # Expected: (batch_size, num_positions, num_positions)
+    print("- Value (V):", value.shape)  # Expected: (batch_size, num_positions, value_depth)
+    print("- Output:", output.shape)    # Expected: (batch_size, num_positions, value_depth)
+    
+    print("\nValues:")
+    # The following matrices will have random values based on the input, so no specific expected values can be provided.
+    print("- Dot Product (Q.K^T):\n", matmul_qk)
+    print("- Scaled Dot Product:\n", logits)
+    print("- Attention Weights:\n", attention_weights)
+    print("- Output:\n", output)
     
     return output, attention_weights
 
@@ -58,13 +56,17 @@ query = np.random.rand(batch_size, num_positions, depth)
 key = np.random.rand(batch_size, num_positions, depth)
 value = np.random.rand(batch_size, num_positions, value_depth)
 
-print("batch_size:", batch_size)
-print("num_positions:", num_positions)
-print("depth:", depth)
-print("value_depth:", value_depth)
-print("-----------------------------")
-print("Query matrix:\n", query)
-print("\nKey matrix:\n", key)
-print("\nValue matrix:\n", value)
+print("Parameters:")
+print("- Batch Size:", batch_size)  # Expected: 2
+print("- Number of Positions:", num_positions)  # Expected: 4
+print("- Depth:", depth)  # Expected: 5
+print("- Value Depth:", value_depth)  # Expected: 6
 
+print("\nInput Matrices:")
+# The following matrices will have random values, so no specific expected values can be provided.
+print("- Query matrix:\n", query)
+print("- Key matrix:\n", key)
+print("- Value matrix:\n", value)
+
+print("\nResults:")
 output, attention_weights = scaled_dot_product_attention(query, key, value)
