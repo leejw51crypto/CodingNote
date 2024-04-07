dc156bd64167a5df226d11108fc0445801a34f1c
jonghwan <jonghwan@crypto.com>
2024-04-07 21:30:08 +0900

    time

--- test-time/src/main.rs
+++ test-time/src/main.rs
@@ -1,6 +1,12 @@
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 fn main() -> anyhow::Result<()> {
+    // time with nanoseconds
+    let timestamp1 = SystemTime::now().duration_since(UNIX_EPOCH)?.as_nanos() as i64;
+    let timestamp2 = chrono::Utc::now().timestamp_nanos_opt().unwrap();
+    println!("timestamp1: {}", timestamp1);
+    println!("timestamp2: {}", timestamp2);
+
     let timestamp1 = SystemTime::now().duration_since(UNIX_EPOCH)?.as_micros() as i64;
     let timestamp2 = chrono::Utc::now().timestamp_micros();
     println!("timestamp1: {}", timestamp1);
@@ -16,5 +22,11 @@ fn main() -> anyhow::Result<()> {
     let timestamp2 = chrono::Utc::now().timestamp();
     println!("timestamp1: {}", timestamp1);
     println!("timestamp2: {}", timestamp2);
+
+
+    let currenttime= chrono::Utc::now();
+    let currentlocaltime= chrono::Local::now();
+    println!("currenttime: {}", currenttime);
+    println!("currentlocaltime: {}", currentlocaltime);
     Ok(())
 }
