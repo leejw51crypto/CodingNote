9a71da3a09d2d27c8411a16bca77bbadcf72e0dc
jonghwan <jonghwan@crypto.com>
2024-04-07 17:29:29 +0900

    ok

--- test-time/src/main.rs
+++ test-time/src/main.rs
@@ -5,5 +5,16 @@ fn main() -> anyhow::Result<()> {
     let timestamp2 = chrono::Utc::now().timestamp_micros();
     println!("timestamp1: {}", timestamp1);
     println!("timestamp2: {}", timestamp2);
+    // the same but with milliseconds
+    let timestamp1 = SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis() as i64;
+    let timestamp2 = chrono::Utc::now().timestamp_millis();
+    println!("timestamp1: {}", timestamp1);
+    println!("timestamp2: {}", timestamp2);
+
+    // the same but with seconds
+    let timestamp1 = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as i64;
+    let timestamp2 = chrono::Utc::now().timestamp();
+    println!("timestamp1: {}", timestamp1);
+    println!("timestamp2: {}", timestamp2);
     Ok(())
 }
