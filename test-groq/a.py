b2b60fdf9e38e10cd08011fb42fde8091fd6f8cd
jonghwan <jonghwan@crypto.com>
2024-04-13 12:28:29 +0900

    hello world

---
+++ test-groq/a.py
@@ -0,0 +1,24 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    # This is the default and can be omitted
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "you are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="mixtral-8x7b-32768",
+)
+
+print(chat_completion.choices[0].message.content)
