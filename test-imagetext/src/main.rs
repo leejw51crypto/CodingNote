d4f22d2a554f6e48c22a8763017a4122dc11903d
jonghwan <jonghwan@crypto.com>
2024-07-17 10:31:21 +0900

    feat: Add viuer dependency for image printing

---
+++ test-imagetext/src/main.rs
@@ -0,0 +1,19 @@
+// src/main.rs
+use viuer::{print_from_file, Config};
+
+fn main() {
+    let conf = Config {
+        // set offset
+     //   x: 20,
+      //  y: 4,
+        // set dimensions
+        width: Some(80),
+        height: Some(25),
+        ..Default::default()
+    };
+
+    // starting from row 4 and column 20,
+    // display `img.jpg` with dimensions 80x25 (in terminal cells)
+    // note that the actual resolution in the terminal will be 80x50
+    print_from_file("img.jpg", &conf).expect("Image printing failed.");
+}
