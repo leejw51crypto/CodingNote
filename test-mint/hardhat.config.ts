41db4753333a2902aa96016ff4183537236d227f
jonghwan <jonghwan@crypto.com>
2024-08-12 16:40:12 +0900

    chore: Update Solidity compiler versions in hardhat.config.ts

--- test-mint/hardhat.config.ts
+++ test-mint/hardhat.config.ts
@@ -2,7 +2,22 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.2',
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.6',
+      },
+      {
+        version: '0.6.2',
+      },
+      {
+        version: '0.5.0',
+      },
+      {
+        version: '0.8.2',
+      },
+    ],
+  },
   networks: {
     my: {
       gas: 'auto',
